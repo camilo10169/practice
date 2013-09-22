3cc0d6efa4f264fe35aaf059ea7fce6ffb2b463d
Juan-se <juan-se@juanse-VGN-FW31J.(none)>
2013-09-22 16:43:28 -0500

    Segunda

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Practice::Application.routes.draw do
+  resources :tables
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
